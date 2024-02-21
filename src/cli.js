4bbb527adc79b1e4928a2c17dbb93987f5e27e34
svetlana <lannatsoy@gmail.com>
2024-02-21 12:46:27 +0900

    changed files

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 
-export const name = readlineSync.question("May I have your name?");
+export const name = readlineSync.question('May I have your name?');
 console.log(`Hello, ${name}!`);
